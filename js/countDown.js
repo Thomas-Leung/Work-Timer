275ccc9b14f79d425207d2e4694bd487b7bb9574
thomas <thomaselmoleung@gmail.com>
2020-02-09 13:24:40 -0500

    added notification feature

--- js/countDown.js
+++ js/countDown.js
@@ -1,38 +1,39 @@
+var path = require('path');
 var minInSec, seconds, distance;
+
+var options = [{
+    title: "Time is up",
+    body: "Time to relax 20s from your work session."
+}]
 
 function setTime() {
     distance = 0;
     minInSec = parseInt(document.getElementById('minutes').value * 60, 10); // base 10
-    console.log(minInSec, 'ok');
     seconds = parseInt(document.getElementById('seconds').value, 10);
-    console.log(seconds);
 
     distance = minInSec + seconds;
-    updateTime();
+    if (!isNaN(distance)) {
+        updateTime();
+    }
 }
 
 function startTimer() {
     // Update the count down every 1 second
     var x = setInterval(function () {
-
-        console.log(distance);
-        updateTime();
-        distance -= 1;
-        // If the count down is over, write some text 
+        // If the count down is over, notify
         if (distance < 0) {
             clearInterval(x);
-            // document.getElementById("time").innerHTML = "EXPIRED";
+            new Notification(options[0].title, options[0]);
+        } else {
+            updateTime();
+            distance -= 1;
         }
     }, 1000);
 }
 
 function updateTime() {
-    console.log(distance, 'disa')
     var minutes = Math.floor(distance / 60);
-    console.log('min', minutes);
     var seconds = distance % 60;
-
-    console.log(seconds, 's');
     // Output the result in an element with id="time"
     document.getElementById("time").innerHTML = minutes + "m " + seconds + "s ";
 }
